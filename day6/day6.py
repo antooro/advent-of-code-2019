6d409adc6b6fd9a9e0e9d9f2a217af2af74bf47c
antooro <antoorodev@gmail.com>
2019-12-06 14:16:54 +0100

    Day 6

---
+++ day6/day6.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+import networkx as nx
+
+with open("input6") as f:
+    datos = f.read().splitlines()
+
+#print(datos)
+
+diccionario = defaultdict(list)
+
+for d in datos:
+    linea = d.split(")")
+
+    diccionario[linea[0]].append(linea[1])
+
+
+
+g = nx.Graph()
+g.add_nodes_from(diccionario.keys())
+for k, v in diccionario.items():
+    g.add_edges_from(([(k, t) for t in v]))
+
+
+
+print("PART 1") 
+valores = set()
+for k,v in diccionario.items():
+    valores.add(k)
+    for val in v:
+        valores.add(val)
+contador = 0
+for value in valores:
+    contador +=(nx.shortest_path_length(g,source="COM",target=value))
+
+print(contador)
+
+
+print("PART 2") 
+print(   nx.shortest_path_length(g,source="SAN",target="YOU") -2)
